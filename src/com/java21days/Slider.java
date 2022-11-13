debb6ad300d9617d3338f7b27a227169f9ecb140
Alejandro Salas <asalas.wd@outlook.com>
2022-11-12 21:53:17 -0500

    Generate constructor

--- src/com/java21days/Slider.java
+++ src/com/java21days/Slider.java
@@ -1,4 +1,19 @@
 package com.java21days;
 
-public class Slider {
+import javax.swing.*;
+import java.awt.*;
+
+public class Slider extends JFrame {
+    Dimension dim = new Dimension(300, 300);
+
+    Slider(String title, int min, int max, int current){
+        super(title);
+        JPanel panel = new JPanel();
+        setSize(dim);
+
+        add(panel);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
 }
